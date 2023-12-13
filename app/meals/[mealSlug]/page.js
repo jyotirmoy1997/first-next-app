bb8658fd751db2321634f0d8d5eb506808eca2c5
jyotirmoy1997 <subhodas45036@gmail.com>
2023-12-13 14:01:59 +0530

    Added Share Meal Page, Added Dynamic Routing for Meals

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,3 +1,36 @@
-export default function MealDetailsPage() {
-  return <h1>Meal Details</h1>;
+import classes from "./page.module.css"
+import Image from "next/image"
+import { getMeal } from "@/lib/meals"
+import { notFound } from "next/navigation"
+
+export default function MealDetailsPage({params}) {
+  const meal = getMeal(params.mealSlug)
+  if(!meal){
+    notFound()
+  }
+  return(
+  <>
+      <header className={classes.header}>
+          <div className={classes.image}>
+              <Image src={meal.image} fill/>
+          </div>
+          <div className={classes.headerText}>
+              <h1>{meal.title}</h1>
+          </div>
+          <p className={classes.creator}>
+              by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+      </header>
+      <main>
+          <p className={classes.instructions}
+              dangerouslySetInnerHTML={{
+                __html : meal.instructions
+              }}
+          >
+          </p>
+      </main>
+  </>
+  )
+  
 }
--- app/meals/page.js
+++ app/meals/page.js
@@ -3,9 +3,14 @@ import Link from 'next/link';
 import classes from './page.module.css';
 import MealsGrid from '@/components/meals/meals-grid';
 import { getMeals } from '@/lib/meals';
+import { Suspense } from 'react';
+
+async function Meals(){
+  const meals = await getMeals()
+  return <MealsGrid meals={meals} />
+}
 
 export default function MealsPage() {
-  const meals = getMeals()
   return (
     <>
       <header className={classes.header}>
@@ -23,7 +28,9 @@ export default function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense fallback={<p>Loading Meals...</p>}>
+          <Meals/>
+        </Suspense>
       </main>
     </>
   );
